645df4b768d4ab5a5364afa3dbb9b357c2152995
redshamn12 <9pvsatria@gmail.com>
2022-04-26 22:23:47 +0700

    Add sumTotal, if for checking < 0 and != number, else if for firstNumber > secondNumber vice versa

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let firstNumber = start;
+    let secondNumber = end;
+    let sumTotal = 0;
+    if ( firstNumber < 0 || secondNumber < 0 || typeof(firstNumber, secondNumber) != "number" ) return "ERROR";
+    else if ( firstNumber > secondNumber){
+        let counter = firstNumber + secondNumber;
+        sumTotal = (firstNumber * counter) / 2;
+        return sumTotal;
+    } else if ( firstNumber < secondNumber){
+        let counter = secondNumber + firstNumber;
+        sumTotal = (secondNumber * counter) / 2;
+        return sumTotal;
+    }
 };
 
 // Do not edit below this line
