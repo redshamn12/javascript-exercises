69d9fd0b48271f4366c55ade54d96cbf155811f3
redshamn12 <9pvsatria@gmail.com>
2022-04-27 00:06:52 +0700

    Complete all function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(numberOne, numberTwo) {
+  return numberOne + numberTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numberOne, numberTwo) {
+	return numberOne - numberTwo
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const totalArr = arr.reduce((total, num) => total + num , 0);
+  return totalArr;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const totalArr = arr.reduce((total, num) => total * num);
+  return totalArr;
 };
 
-const power = function() {
-	
+const power = function(numberOne, numberTwo) {
+	return Math.pow(numberOne, numberTwo);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0 || number === 1){
+    return 1;
+  }
+  for (i = number - 1; i >= 1; i--){
+    number *= i;
+  }
+	return number;
 };
 
 // Do not edit below this line
