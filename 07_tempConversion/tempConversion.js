16e5952b7c6864b0d94898632fbcfb8c76bf0061
redshamn12 <9pvsatria@gmail.com>
2022-04-26 23:20:10 +0700

    Add ftoc = ((F-32)*5)/9 and ctof = (C*9/5)+32

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(deg) {
+  let totalVal =  ((deg - 32) * 5) / 9 ;
+  if (Number.isInteger(totalVal) === false){
+    return parseFloat(totalVal.toFixed(1));
+  } else return parseFloat(totalVal.toFixed(1));;
 };
 
-const ctof = function() {
-
+const ctof = function(deg) {
+  let totalVal = (deg * 9 / 5) + 32;
+  if (Number.isInteger(totalVal) === false){
+    return parseFloat(totalVal.toFixed(1));
+  } else return parseFloat(totalVal.toFixed(1));;
 };
 
 // Do not edit below this line
